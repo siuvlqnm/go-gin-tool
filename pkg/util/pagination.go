6b0c241ad4f98430baf4a39c2a9e06f5f28ff2ad
siuvlqnm <siuvlqnm@gmail.com>
2020-08-05 14:22:05 +0800

    demo

--- pkg/util/pagination.go
+++ pkg/util/pagination.go
@@ -2,7 +2,16 @@ package util
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/unknwon/com"
 	"github.com/siuvlqnm/go-gin-example/pkg/setting"
+	"github.com/unknwon/com"
 )
 
+func GetPage(c *gin.Context) {
+	result := 0
+	page, _ := com.StrTo(c.Query("page")).Int()
+	if page > 0 {
+		result = (page - 1) * setting.PageSize
+	}
+
+	return result
+}
